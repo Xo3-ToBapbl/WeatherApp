3a54a148eba08f8fae5b97cff759b37b971ff2e7
Ivan Stupak <i.stupak@godeltech.com>
2022-07-21 20:01:56 +0300

    content toolbar added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,8 @@ import printMe from './print.js';
 document.querySelector('.search-btn').onclick = () => swapSidePanel(180);
 document.querySelector('.swap-panel-btn').onclick = () => swapSidePanel(0);
 
+new ButtonToggler('.theme-mode-btn', switchTheme);
+
 function swapSidePanel(angle) {
   let container = document.querySelector('.side-panels-container');
   let sidePanelsInner = document.querySelector('.side-panels-inner');
@@ -12,4 +14,32 @@ function swapSidePanel(angle) {
     setTimeout(() => container.style.overflow = '', 300);
   }
   sidePanelsInner.style.transform = `rotateY(${angle}deg)`;
+}
+
+function ButtonToggler(selector, callback) {
+  this.toggleButtons = [...document.querySelectorAll(selector)];
+  this.toggleButtons.forEach((btn) => {
+    btn.addEventListener('click', buttonToggled.bind(this));
+    btn.addEventListener('click', callback);
+  })
+
+  function buttonToggled() {
+    let targetBtn = event.target;
+    if (targetBtn.dataset.toggled === 'true') {
+      return;
+    }
+
+    let currentToggledBtn = this.toggleButtons.find((btn) => btn.dataset.toggled === 'true');
+    currentToggledBtn.setAttribute('data-toggled', 'false');
+    targetBtn.setAttribute('data-toggled', 'true');
+  }
+}
+
+function switchTheme() {
+  let toggledBtn = event.target;
+  if (toggledBtn.dataset.toggled === 'false') {
+    return;
+  }
+
+  document.documentElement.setAttribute('data-theme', toggledBtn.dataset.theme);
 }

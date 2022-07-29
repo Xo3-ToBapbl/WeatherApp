a4a373ad72417589062212a2e96ece16bbafe63c
Ivan Stupak <i.stupak@godeltech.com>
2022-07-29 18:12:31 +0300

    firebase added

--- src/app/app.js
+++ src/app/app.js
@@ -1,16 +1,38 @@
 import "$blocks/_blocks.js";
+import { initializeApp } from "firebase/app";
 
 (()=> {
   
-  document.addEventListener("onSwitchTheme", switchTheme);
-  document.addEventListener("onSwitchTemperatureUnits", switchTemperatureUnit);
+  new Application();
 
-  function switchTheme() {
-    document.documentElement.setAttribute("data-theme", event.detail.theme);
-  }
-
-  function switchTemperatureUnit() {
-    console.log("Temperature units switched");
-  }
+  function Application() {
+    initializeFirebase();
+    addEventListeners();
   
+    function initializeFirebase() {
+      const firebaseConfig = {
+        apiKey: "AIzaSyBGZ3W4Kg67RWJ3-Ot4vdDLegsYx-x4bj0",
+        authDomain: "weatherapp-9fe6e.firebaseapp.com",
+        projectId: "weatherapp-9fe6e",
+        storageBucket: "weatherapp-9fe6e.appspot.com",
+        messagingSenderId: "1061920221511",
+        appId: "1:1061920221511:web:0b067ef12549ad71c68462"
+      };
+  
+      initializeApp(firebaseConfig);
+    }
+  
+    function addEventListeners() {
+      document.addEventListener("onSwitchTheme", switchTheme);
+      document.addEventListener("onSwitchTemperatureUnits", switchTemperatureUnit);
+    }
+    
+    function switchTheme() {
+      document.documentElement.setAttribute("data-theme", event.detail.theme);
+    }
+  
+    function switchTemperatureUnit() {
+      console.log("Temperature units switched");
+    }
+  }
 })();
